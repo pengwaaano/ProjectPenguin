a9917689317988f54b9c446bbc40cee287fccc86
Jacob Greenland <>
2018-10-08 12:24:40 +0100

    Factory can now be upgraded to increase production.

--- Assets/Controllers/FactoryController.cs
+++ Assets/Controllers/FactoryController.cs
@@ -8,32 +8,45 @@ using UnityEngine.UI;
 public class FactoryController : MonoBehaviour
 {
     public Text factoryCount;
-    
+
     public float outputPerSecond;
     public float timeToProcess;
     public float fish;
     public float level;
+    public float baseCost;
+    public float coefficient;
 
     private CurrencyController cController;
+
     void Start()
     {
         cController = gameObject.GetComponent<CurrencyController>();
         InvokeRepeating("processFish", 0, 1);
     }
-    
+
     void Update()
     {
         factoryCount.text = Util.CurrencyToString(fish);
     }
-    
+
     public void addFish(float _fish)
     {
         fish += _fish;
     }
 
+    public float getCost()
+    {
+        return baseCost * Mathf.Pow(coefficient, level);
+    }
+
+    private float getOutputPerSecond(float multiplier)
+    {
+        return (outputPerSecond * level) * multiplier;
+    }
+
     private void processFish()
     {
-        float fishToProcess = fish > outputPerSecond ? outputPerSecond : fish;
+        float fishToProcess = fish > getOutputPerSecond(1) ? getOutputPerSecond(1) : fish;
         StartCoroutine(finishProcess(fishToProcess));
     }
 
@@ -42,5 +55,14 @@ public class FactoryController : MonoBehaviour
         yield return new WaitForSeconds(timeToProcess);
         cController.addFish(f);
         fish -= f;
+    }
+
+    public void upgradeFactory()
+    {
+        if (cController.canAfford(getCost()))
+        {
+            cController.spendFish(getCost());
+            level += 1;
+        }
     }
 }
