06c7df14f6c0b5b1332516d2af261bd53165d036
Henry Tsang <henry.tsang@city-holdings.co.uk>
2018-10-12 15:49:59 +0100

    Added fish spawn and moving.

---
+++ Assets/Classes/MoveFish.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFish : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+        Destroy(this.gameObject, 5.0f);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        transform.position = new Vector3(this.transform.position.x + 1.0f, this.transform.position.y);
+		
+	}
+}
---
+++ Assets/Classes/SpawnFish.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFish : MonoBehaviour {
+
+    public GameObject fish; //fishy
+    public float spawnTime = 1f; //spawn time in seconds
+    public Transform spawnPoint;
+    public bool isSpawning = false;
+    public bool shouldSpawn = true; //Should always be set to True after user has one working penguin
+
+    public GameObject parentObject;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    IEnumerator Spawn (float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        GameObject Fishy = Instantiate(fish, spawnPoint.transform.position, spawnPoint.transform.rotation);
+
+        Fishy.transform.parent = parentObject.transform;
+
+        isSpawning = false;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+
+        if(!isSpawning && shouldSpawn == true)
+        {
+            isSpawning = true;
+            StartCoroutine(Spawn(2.0f));
+        }
+    }
+}
