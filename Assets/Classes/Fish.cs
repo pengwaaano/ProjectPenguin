dffedd70de2d9af851f8ece156a114a30c8712c3
Henry Tsang <henry.tsang@city-holdings.co.uk>
2018-10-19 14:09:33 +0100

    Preserved aspect on ui elements

--- Assets/Classes/Fish.cs
+++ Assets/Classes/Fish.cs
@@ -4,25 +4,10 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour {
 
-    public GameObject fish; //fishy
-    public float spawnTime = 1f; //spawn time in seconds
-    public Transform spawnPoint;
-    public bool spawnOn = false;
-
 	// Use this for initialization
 	void Start () {
-
-        InvokeRepeating("Fishy", spawnTime, spawnTime);
 		
 	}
-
-    void Spawn ()
-    {
-        if (spawnOn)
-        {
-            Instantiate(fish, spawnPoint.position, spawnPoint.rotation);
-        }
-    }
 	
 	// Update is called once per frame
 	void Update () {
